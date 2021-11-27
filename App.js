1ceaf05c8317732042310d96d6651491cf4b0515
meohyun24 <eogus1954@naver.com>
2021-11-27 21:38:46 +0900

    modified 3

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import MainPage from './pages/MainPage'
 import AboutPage from './pages/AboutPage'
+import DetailPage from './pages/DetailPage'
 
 
 // mainpage 통째로 가져옴 / 컴포넌트화, 페이지 분할
 export default function App(){
-  //return (<MainPage/>)
-  return(<AboutPage/>)
+  return (<MainPage/>)
+  // return(<DetailPage/>)
 }
